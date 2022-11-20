501dc2eddc3dcebff21f2f8233310aace204c0eb
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2022-11-20 17:39:33 +0100

    Fixes to comply with some pylint suggestions

--- game_of_life.py
+++ game_of_life.py
@@ -1,7 +1,11 @@
+"""An implementation of the Game of Life."""
+
+
+import sys
 import time
 
 import pygame
-from pygame.locals import *
+from pygame.locals import K_RETURN, KEYDOWN, QUIT
 
 WIDTH = 700
 HEIGHT = 700
@@ -12,45 +16,45 @@ def get_alive(matrix):
     return count
 
 
-def is_valid(x, y, n):
-    return min(x, y) >= 0 and max(x, y) < n
+def is_valid(x_cord, y_cord, size):
+    return min(x_cord, y_cord) >= 0 and max(x_cord, y_cord) < size
 
 
 def handle(event):
     if event.type == QUIT:
         pygame.quit()
-        quit()
+        sys.exit()
 
 
-def draw(matrix, n):
+def draw(matrix, size):
 
     black_color = (0, 0, 0)
     grey_color = (105, 105, 105)
     white_color = (255, 255, 255)
     game_display.fill(white_color)
 
-    for i in range(n):
-        for j in range(n):
-            if matrix[i][j] == 1:
-                rect = pygame.Rect(j * (HEIGHT // n), i *
-                                   (HEIGHT // n), HEIGHT // n, HEIGHT // n)
+    for row in range(size):
+        for col in range(size):
+            if matrix[row][col] == 1:
+                rect = pygame.Rect(col * (HEIGHT // size), row *
+                                   (HEIGHT // size), HEIGHT // size, HEIGHT // size)
                 pygame.draw.rect(game_display, black_color, rect)
 
-    for i in range(n + 1):
+    for row in range(size + 1):
         pygame.draw.line(game_display, grey_color,
-                         (i * (HEIGHT // n), 0), (i * (HEIGHT // n), WIDTH), width=1)
+                         (row * (HEIGHT // size), 0), (row * (HEIGHT // size), WIDTH), width=1)
 
-    for i in range(n + 1):
+    for col in range(size + 1):
         pygame.draw.line(game_display, grey_color,
-                         (0, i * (WIDTH // n)), (HEIGHT, i * (WIDTH // n)), width=1)
+                         (0, col * (WIDTH // size)), (HEIGHT, col * (WIDTH // size)), width=1)
 
     pygame.display.update()
 
 
-def setup(matrix, n):
+def setup(matrix, size):
     print("Place initial disposition of alive cells")
 
-    draw(matrix, n)
+    draw(matrix, size)
 
     initial_setup = True
     while initial_setup:
@@ -60,17 +64,17 @@ def setup(matrix, n):
 
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                i = pos[1] // (HEIGHT // n)
-                j = pos[0] // (HEIGHT // n)
+                i = pos[1] // (HEIGHT // size)
+                j = pos[0] // (HEIGHT // size)
                 matrix[i][j] = 1
-                draw(matrix, n)
+                draw(matrix, size)
 
 
 if __name__ == '__main__':
 
-    n = int(input('Enter dimension of the system: '))
+    size = int(input('Enter dimension of the system: '))
 
-    matrix = [[0] * n for _ in range(n)]
+    matrix = [[0] * size for _ in range(size)]
 
     dx = [1, 1, -1, -1, 0, 0, 1, -1]
     dy = [1, -1, 1, -1, 1, -1, 0, 0]
@@ -79,35 +83,35 @@ if __name__ == '__main__':
     game_display = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.flip()
 
-    setup(matrix, n)
+    setup(matrix, size)
 
     while get_alive(matrix) > 0:
 
         for event in pygame.event.get():
             handle(event)
-        draw(matrix, n)
+        draw(matrix, size)
 
-        transition = [[0] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
+        transition = [[0] * size for _ in range(size)]
+        for row in range(size):
+            for col in range(size):
                 alive = 0
                 for k in range(8):
-                    nx = i + dx[k]
-                    ny = j + dy[k]
-                    if is_valid(nx, ny, n):
+                    nx = row + dx[k]
+                    ny = col + dy[k]
+                    if is_valid(nx, ny, size):
                         alive += matrix[nx][ny]
-                if matrix[i][j] == 1 and alive in [2, 3]:
-                    transition[i][j] = 1
-                if matrix[i][j] == 0 and alive == 3:
-                    transition[i][j] = 1
+                if matrix[row][col] == 1 and alive in [2, 3]:
+                    transition[row][col] = 1
+                if matrix[row][col] == 0 and alive == 3:
+                    transition[row][col] = 1
 
-        for i in range(n):
-            for j in range(n):
-                matrix[i][j] = transition[i][j]
+        for row in range(size):
+            for col in range(size):
+                matrix[row][col] = transition[row][col]
 
         time.sleep(0.2)
 
     for event in pygame.event.get():
         handle(event)
-    draw(matrix, n)
+    draw(matrix, size)
     time.sleep(1)
